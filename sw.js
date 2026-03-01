b42cce0285fa3073178a89b9441edce5f78893ff
suguitani-creator <leandrosuguitani@gmail.com>
2026-03-01 18:44:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,33 @@
+const CACHE_NAME = "aurea-v4";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/firebase.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
 self.addEventListener("install", event => {
-  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  console.log("Service Worker ativado");
   event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
