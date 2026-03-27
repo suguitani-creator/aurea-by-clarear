e928133ad4ca331dcae744e5e1f8bf21c5b1c19d
suguitani-creator <leandrosuguitani@gmail.com>
2026-03-27 00:39:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -256,7 +256,7 @@ async function adicionarTransacao() {
         }
 
         // Se for um "Investimento", não é necessário preencher categoria e subcategoria
-        if (tipo === "despesa" && categoria !== "Investimentos") {
+        if (tipo === "despesa" && essencial !== "investimento") {
             if (!categoria || !subcategoria) {
                 showToast("Preencha categoria e subcategoria", "error");
                 return;
