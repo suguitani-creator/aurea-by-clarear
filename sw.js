12b8d7d4f6e87af07e2446118341c909ebf6009c
suguitani-creator <leandrosuguitani@gmail.com>
2026-04-08 20:58:26 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -34,8 +34,19 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
+      .then(cachedResponse => {
+        if (cachedResponse) return cachedResponse;
+
+        return fetch(event.request)
+          .then(networkResponse => {
+            return caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            });
+          });
+      })
+      .catch(() => {
+        return new Response("Sem conexão", { status: 503 });
       })
   );
 });
